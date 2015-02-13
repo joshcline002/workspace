832c490e14ae2f2f23e2b46ca09eb8b0a8206fb8
Joshua C Cline <joshcline002@gmail.com>
2015-02-13 11:52:26 -0600

    339 Assign 3
    Made an interface Class Items and associated other classes(movies,
    music, cds, dvds, books)

---
+++ SE329Assign3/src/Music.java
@@ -0,0 +1,39 @@
+
+public class Music implements Item {
+	
+	 private String _title;
+	    private int    _priceCode;
+	    
+	    public Music(String title, int priceCode) {
+	        _title = title;
+	        _priceCode = priceCode;
+	    }
+
+	@Override
+	public int getPriceCode() {
+		return _priceCode;
+	}
+
+	@Override
+	public void setPriceCode(int arg) {
+		_priceCode = arg;
+	}
+
+	@Override
+	public String getTitle() {
+		return _title;
+	}
+
+	@Override
+	public double getCost(int daysRented) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPoints(int daysRented) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
