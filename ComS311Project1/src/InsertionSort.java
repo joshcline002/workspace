6883f87a121b14e1ed01a042f3a25a0232eefa46
Joshua C Cline <joshcline002@gmail.com>
2015-02-25 13:45:44 -0600

    Hateful Hateful
    Don't you wanna, wanna be hateful

--- ComS311Project1/src/InsertionSort.java
+++ ComS311Project1/src/InsertionSort.java
@@ -12,17 +12,19 @@ public class InsertionSort<E extends Comparable<? super E>> implements  SortAnal
 	
 	@Override
 	public long analyzeSort(ArrayList<E> list) {
+		ArrayList<E> clone = new ArrayList<E>(list.size());
+	    for(E item: list) clone.add((E) item);
 		startTime = System.currentTimeMillis();
 		int i, j;
-		for(i = 1; i< list.size(); i++){
-			E obj = list.get(i);
+		for(i = 1; i< clone.size(); i++){
+			E obj = clone.get(i);
 			j = i;
 			
-			while(j > 0 && 1 == list.get(j-1).compareTo(list.get(i))){
-				list.set(j, list.get(j-1));
+			while(j > 0 && 1 == clone.get(j-1).compareTo(clone.get(i))){
+				clone.set(j, clone.get(j-1));
 				j--;
 			}
-			list.set(j, obj);
+			clone.set(j, obj);
 		}
 		endTime = System.currentTimeMillis();
 		return endTime - startTime;
