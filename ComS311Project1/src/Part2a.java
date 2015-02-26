e1bbb1f693c693b2ad2f7a17b1f5d786dcef6ff6
Joshua C Cline <joshcline002@gmail.com>
2015-02-26 17:10:36 -0600

    311
    Almost done with Project 1 part 3

--- ComS311Project1/src/Part2a.java
+++ ComS311Project1/src/Part2a.java
@@ -11,7 +11,7 @@ public class Part2a {
 		boolean found = true;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		int size = 0;
-		long time;
+		long time = 0;
 		int j = 0;
 		int numTimes=0;
 		int avgSize = 0;
@@ -21,38 +21,58 @@ public class Part2a {
 		BufferedWriter bw2 = new BufferedWriter(new FileWriter(g)); 
 		InsertionSort sort = new InsertionSort();
 		Random ranNum = new Random();
-		while(numTimes<5){
+		int max;
+		int min = 0;
+		int mid;
 		int i = 2;
 		found = true;
 		while(found){
-			while(j<i){
-				try{
+			list.clear();
+			for(j =0; j< i; j++){
 				list.add(ranNum.nextInt());
-				j++;
-				}catch (OutOfMemoryError e){
-				}
 			}
 			time = sort.analyzeSort(list);
-
+			System.out.println("size =" + i + "Time =" + time);
 			if(time > 1000){
 				found = false;
 				size = i;
-			} else if( time > 500 ){
-				i++;
-			}else{
+			} else {
 				i = i*2;
 			}
 		}
+			found = true;
+			max = size;
+			while(found){
+				if(max < min){
+					found = false;
+				} else{
+					mid = (max - min)/2 + min;
+					list.clear();
+					for(int k = 0; k<mid; k++){
+						list.add(ranNum.nextInt());
+					}
+					time = sort.analyzeSort(list);
+					System.out.println("BS time" + time);
+					if(time>1002){
+						size = mid;
+						max = mid+1;
+					} else if(time<998){
+						size = mid;
+						min = mid-1;
+					} else {
+						size = mid;
+						found = false;
+					}
+				}
+			list.clear();
+			j=0;
+		}
 		list.clear();
 		j = 0;
-		System.out.println("Found size =" + size);
-		avgSize += size;
-		numTimes++;
-	}
+		System.out.println("Found size =" + size + "Time =" + time);
 		int timing;
-		size = avgSize/numTimes;
-		System.out.println("Found Average Size =" + size);
-		int i = 0;
+		
+		i = 1;
 		for(int k =  0; k<100; k++){
 			while(j<i){
 				list.add(i-j);
@@ -61,23 +81,29 @@ public class Part2a {
 			time = sort.analyzeSort(list); 
 			timing = (int)time;
 			System.out.println("time" + timing + "size" + i);
-			bw.write(timing);
+			bw.write(timing + "");
 			bw.newLine();
-				i = i+size/100;
+			i = i+size/100;
+			list.clear();
+			j = 0;
 				
 		}
 		bw.close();
-		i = 0;
+		i = 1;
+		j = 0;
 		for(int k =  0; k<100; k++){
 			while(j<i){
 				list.add(ranNum.nextInt());
 				j++;
 			}
 			time = sort.analyzeSort(list); 
-			bw2.write((int) time);
+			timing = (int)time;
+			System.out.println("time" + timing + "size" + i);
+			bw2.write(timing + "");
 			bw2.newLine();
-				i = i+size/100;
-				
+			i = i+size/100;
+			list.clear();
+			j = 0;
 		}
 		bw2.close();
 	}
