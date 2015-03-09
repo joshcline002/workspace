360180f86a45f2e80eb34bcf6b5881da1e38c4f4
Joshua C Cline <joshcline002@gmail.com>
2015-03-09 15:40:43 -0500

    339 Assign 4
    State design pattern for a simple calculator

---
+++ SE329Assign4/src/NumberState.java
@@ -0,0 +1,10 @@
+
+public class NumberState implements State {
+
+	@Override
+	public void doAction(CalcContext context, String input) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SE329Assign4/src/OperationState.java
@@ -0,0 +1,10 @@
+
+public class OperationState implements State {
+
+	@Override
+	public void doAction(CalcContext context, String input) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
